f8d4044225539fd28d42fafaf922028a5683ed3f
Per Sandström <pg.sandstrom@gmail.com>
2020-08-09 13:25:45 +0200

    Fix some release script errors

--- do_complete_deploy.sh
+++ do_complete_deploy.sh
@@ -6,20 +6,25 @@ cd "$(dirname "$0")"
 
 ./prep_repo_for_deploy.sh
 
-npm install --only=prod
+# TODO maybe move typescript stuff to dependency and then run this instead?
+# npm install --only=prod
+npm install
 
 npm run build
+
+rm -rf ./release
+mkdir ./release
 
 mv dist release
 mv .next release
 cp -r pm2.json ./release
 cp -r ./node_modules ./release/
 
-# is next-env.d.ts needed, what does it do?
+# TODO is next-env.d.ts needed, what does it do?
 
 mkdir -p /apps/test.codeandstuff.se
 
-mv relese/* /apps/test.codeandstuff.se
+rsync -a release/* /apps/test.codeandstuff.se
 
 # find a neat solution to not only restart pm2, but to add if it is not already there
 pm2 restart plox-notes-2
