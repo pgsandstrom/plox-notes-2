8bcf266d13c184584a7efb6fedc8e3258f9a9183
Per Sandström <pg.sandstrom@gmail.com>
2020-08-09 13:43:51 +0200

    Fix release script errors

--- do_complete_deploy.sh
+++ do_complete_deploy.sh
@@ -12,19 +12,11 @@ npm install
 
 npm run build
 
-rm -rf ./release
-mkdir ./release
-
-mv dist release
-mv .next release
-cp -r pm2.json ./release
-cp -r ./node_modules ./release/
-
-# TODO is next-env.d.ts needed, what does it do?
-
 mkdir -p /apps/test.codeandstuff.se
 
-rsync -a release/* /apps/test.codeandstuff.se
+# currently we copy the whole project into the prod folder
+# TODO find out exactly which parts are required, and copy in these
+rsync -a * /apps/test.codeandstuff.se
 
 # find a neat solution to not only restart pm2, but to add if it is not already there
 pm2 restart plox-notes-2
