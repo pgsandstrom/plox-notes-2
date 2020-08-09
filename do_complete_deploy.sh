ca6e4807f2d1ff3d0a71a468b01d8e0e00bda1f8
Per Sandström <pg.sandstrom@gmail.com>
2020-08-09 19:15:00 +0200

    Fix error in release script

--- do_complete_deploy.sh
+++ do_complete_deploy.sh
@@ -16,7 +16,7 @@ mkdir -p /apps/test.codeandstuff.se
 
 # currently we copy the whole project into the prod folder
 # TODO find out exactly which parts are required, and copy in these
-rsync -a * /apps/test.codeandstuff.se
+rsync -a . /apps/test.codeandstuff.se
 
 # find a neat solution to not only restart pm2, but to add if it is not already there
 pm2 restart plox-notes-2
