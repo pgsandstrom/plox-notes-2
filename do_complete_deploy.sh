7183393e2c0be070754fdc50ccab634aa9dcca52
Per Sandström <pg.sandstrom@gmail.com>
2020-08-10 20:06:19 +0200

    Fix so ci server dont need to install devDependencies

--- do_complete_deploy.sh
+++ do_complete_deploy.sh
@@ -7,16 +7,14 @@ cd "$(dirname "$0")"
 ./prep_repo_for_deploy.sh
 # TODO a super cool thing would be if the script could detect that the git update changed this script, and then abort. Or even reload itself.
 
-# TODO maybe move typescript stuff to dependency and then run this instead?
-# npm install --only=prod
-npm install
+npm install --only=prod
 
 npm run build
 
 mkdir -p /apps/test.codeandstuff.se
 
 # currently we copy the whole project into the prod folder
-# TODO find out exactly which parts are required, and copy in these
+# TODO find out exactly which parts are required, and only copy in those
 rsync -a . /apps/test.codeandstuff.se
 
 # find a neat solution to not only restart pm2, but to add if it is not already there
