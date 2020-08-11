c9146b7fbbbbc90758008fbde369c5c461f63f0b
Per Sandström <pg.sandstrom@gmail.com>
2020-08-11 13:12:41 +0200

    v1.0.0

--- do_complete_deploy.sh
+++ do_complete_deploy.sh
@@ -11,11 +11,11 @@ npm install --only=prod
 
 npm run build
 
-mkdir -p /apps/test.codeandstuff.se
+mkdir -p /apps/bos.se
 
 # currently we copy the whole project into the prod folder
 # TODO find out exactly which parts are required, and only copy in those
-rsync -a . /apps/test.codeandstuff.se
+rsync -a . /apps/bos.se
 
 # find a neat solution to not only restart pm2, but to add if it is not already there
 pm2 restart plox-notes-2
