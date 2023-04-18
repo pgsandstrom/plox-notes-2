bb30a3a045585b0fa476fa29208a7d396448db7e
Per Sandström <hello@persandstrom.com>
2023-04-18 21:54:58 +0200

    Upgrade dependencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export default function Home() {
   const router = useRouter()
